de04e2a7e59e52beb662de1bbbd43db03fda60a9
hieu <hieufhhp@gmail.com>
2026-07-19 17:45:10 +0700

    feat(housekeeping): introduce housekeeping task aggregate

--- app/Domain/Housekeeping/Repositories/HousekeepingTaskRepository.php
+++ app/Domain/Housekeeping/Repositories/HousekeepingTaskRepository.php
@@ -2,6 +2,13 @@
 
 namespace Domain\Housekeeping\Repositories;
 
+use Domain\Housekeeping\Aggregates\HousekeepingTask;
+
 interface HousekeepingTaskRepository
 {
+    public function save(HousekeepingTask $task): void;
+
+    public function find(string $id): ?HousekeepingTask;
+
+    public function nextPending(string $unitId): ?HousekeepingTask;
 }
